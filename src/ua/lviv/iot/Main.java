8706f734e8d23497185e96431415ca6f7a0a9eee
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-10-13 16:51:27 +0300

    Solution for task

---
+++ src/ua/lviv/iot/Main.java
@@ -0,0 +1,8 @@
+package ua.lviv.iot;
+
+public class Main {
+    public static void main(String[] args) {
+        FileData fileData = new FileData();
+        System.out.println(fileData.countOfPairs());
+    }
+}
